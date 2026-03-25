66b733a15aefbd69a5a61a8d02d6cf0c9f1a6ba8
eigentaylor <51183393+eigentaylor@users.noreply.github.com>
2026-03-25 19:46:36 +0000

    Deploying to gh-pages from @ eigentaylor/eigentaylor.github.io@5ff08db1bef9ad08dba748c52945ded4ffb346d7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -658,6 +658,11 @@ ninja.data = [{
           description: "An explanation of the leader rule strategy in approval voting, and its positive ramifications.",
           section: "Previews",handler: () => {
               window.location.href = "/preview/avstrategy/";
+            },},{id: "previews-approval-is-a-condorcet-method",
+          title: 'Approval is a Condorcet Method',
+          description: "Approval is the perfect Condorcet method, and I have permanently solved the Condorcet paradox. April Fools!",
+          section: "Previews",handler: () => {
+              window.location.href = "/preview/condorcetapproval/";
             },},{id: "previews-jordan-canonical-form-made-easier-part-1",
           title: 'Jordan Canonical Form Made Easier Part 1',
           description: "not easy. just like... easier...",
