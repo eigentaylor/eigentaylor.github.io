0003bf285840234b55d7f44c90e5e3bfb2729f8c
eigentaylor <51183393+eigentaylor@users.noreply.github.com>
2026-04-10 05:38:05 +0000

    Deploying to gh-pages from @ eigentaylor/eigentaylor.github.io@11852de48c578cdb7cdf4a6e0926a101900ab74b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -659,11 +659,6 @@ ninja.data = [{
           description: "Approval is the perfect Condorcet method, and I have permanently solved the Condorcet paradox. April Fools!",
           section: "Previews",handler: () => {
               window.location.href = "/preview/condorcetapproval/";
-            },},{id: "previews-a-guide-to-approval-voting-strategy",
-          title: 'A Guide to Approval Voting Strategy',
-          description: "An explanation of the leader rule strategy in approval voting, and its positive ramifications.",
-          section: "Previews",handler: () => {
-              window.location.href = "/preview/avstrategy/";
             },},{id: "previews-approval-is-the-perfect-condorcet-method",
           title: 'Approval is the Perfect Condorcet Method',
           description: "Approval is a perfect Condorcet method, and I have permanently solved the Condorcet paradox. April Fools!",
@@ -674,6 +669,11 @@ ninja.data = [{
           description: "Opinion piece about my thoughts on other systems like STAR, RCV, and Condorcet.",
           section: "Previews",handler: () => {
               window.location.href = "/preview/approval-only/";
+            },},{id: "previews-a-guide-to-approval-voting-strategy",
+          title: 'A Guide to Approval Voting Strategy',
+          description: "An explanation of the leader rule strategy in approval voting, and its positive ramifications.",
+          section: "Previews",handler: () => {
+              window.location.href = "/preview/avstrategy/";
             },},{id: "previews-arrow-39-s-impossibility-theorem",
           title: 'Arrow&amp;#39;s Impossibility Theorem',
           description: "Walkthrough and proof of the theorem that proves ranked voting systems cannot satisfy seemingly reasonable properties.",
