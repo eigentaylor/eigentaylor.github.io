c882b0ebed26889ad1f1dea347fe1fa80df470d2
eigentaylor <tayloreigenfisher@gmail.com>
2026-06-06 18:53:46 -0700

    Update purgecss.config.js

--- purgecss.config.js
+++ purgecss.config.js
@@ -3,20 +3,31 @@ module.exports = {
   css: ["_site/assets/css/*.css"],
   output: "_site/assets/css/",
   skippedContentGlobs: ["_site/assets/**/*.html"],
-  safelist: [
-    "collapse",
-    "collapsing",
-    "show",
-    "dropdown-menu",
-    "dropdown-item",
-    "table",
-    "table-dark",
-    "table-hover",
-    "table-responsive",
-    "af-tooltip",
-    "af-popover",
-    "font-weight-bold",
-    "font-weight-medium",
-    "font-weight-lighter",
-  ],
+  safelist: {
+    standard: [
+      "collapse",
+      "collapsing",
+      "show",
+      "dropdown-menu",
+      "dropdown-item",
+      "table",
+      "table-dark",
+      "table-hover",
+      "table-responsive",
+      "af-tooltip",
+      "af-popover",
+      "font-weight-bold",
+      "font-weight-medium",
+      "font-weight-lighter",
+      // Custom navbar classes from header.liquid override
+      "navbar-collapse-main",
+      "navbar-menu-list",
+      "navbar-toggler-main",
+      // Draft preview body class (only on preview pages, may not appear in built _site)
+      "is-preview",
+    ],
+    patterns: [
+      /^navbar/, // keep all navbar-* classes (Bootstrap + custom al-folio)
+    ],
+  },
 };
